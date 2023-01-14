23287f33a981f69e26f94de0644ff1f3d8e4f628
megustaEevee <71437195+megustaEevee@users.noreply.github.com>
2023-01-14 16:15:26 +0900

    basic認証無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  #before_action :basic_auth
 
   private
 
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    #end
+  #end
 end
