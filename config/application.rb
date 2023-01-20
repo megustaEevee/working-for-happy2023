1f0a4a81daa2b872cd507c80004d1915167df271
megustaEevee <71437195+megustaEevee@users.noreply.github.com>
2023-01-21 08:53:51 +0900

    Rakefile追加

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ Bundler.require(*Rails.groups)
 module WorkingForHappy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.active_record.legacy_connection_handling = false
-    config.assets.initialize_on_precompile = false
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
